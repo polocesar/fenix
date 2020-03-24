4cf57129b65a2d28b6c641b65bb0af6cc55ef68d
Neto Barbosa <jbsn2019@gmail.com>
2020-03-24 20:10:13 -0300

    Adicionando o Vuejs

--- start/routes.js
+++ start/routes.js
@@ -16,4 +16,7 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.on('/').render('welcome')
+//Route.on('/').render('welcome');
+
+
+Route.any('*', ({view}) =>  view.render('app'))
